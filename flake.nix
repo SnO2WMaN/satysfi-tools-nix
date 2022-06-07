8adb48250fd297edac3e50f728eaeea18e7a0be4
SnO2WMaN <me@sno2wman.net>
2022-06-08 04:01:52 +0900

    fix overlay in flake.nix

--- flake.nix
+++ flake.nix
@@ -9,28 +9,45 @@
     devshell = {
       url = "github:numtide/devshell";
     };
+
+    satysfi-formatter.url = "github:SnO2WMaN/satysfi-formatter/nix-integrate";
+    satysfi-lsp.url = "github:SnO2WMaN/satysfi-language-server/nix-integrate";
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, devshell, ... } @ inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , devshell
+    , satysfi-formatter
+    , satysfi-lsp
+    , ...
+    } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlay ];
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ devshell.overlay ];
+      };
+    in
+    rec {
+      overlay = (final: prev: {
+        satysfi-tools = {
+          satysfi-formatter = satysfi-formatter.packages."${system}".default;
+          satysfi-language-server = satysfi-formatter.packages."${system}".default;
         };
-      in
-      rec {
-        overlay = import ./overlay.nix;
+      });
 
-        # `nix develop`
-        devShell = pkgs.devshell.mkShell {
-          imports = [
-            (pkgs.devshell.importTOML ./devshell.toml)
-          ];
-        };
-      }
+      # `nix develop`
+      devShell = pkgs.devshell.mkShell {
+        imports = [
+          (pkgs.devshell.importTOML ./devshell.toml)
+        ];
+      };
+    }
     );
 }
